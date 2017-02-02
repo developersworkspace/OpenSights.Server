52fd4eadf047e489fdd531127336d65c2ec212e6
Barend Erasmus <developersworkspace@gmail.com>
2017-02-02 13:35:16 +0200

    Adds data retrieval

--- api/src/routes/data.ts
+++ api/src/routes/data.ts
@@ -10,6 +10,14 @@ router.post('/save', (req: Request, res: Response, next: Function) => {
     res.send('OK');
 });
 
+/*
+    {
+        "userAgent": "$userAgent"
+    }
+*/
+router.post('/get', (req: Request, res: Response, next: Function) => {
+    select(res, req.body);
+});
 
 function saveToDatastore(obj) {
     let mongoClient = new mongodb.MongoClient();
@@ -20,6 +28,28 @@ function saveToDatastore(obj) {
             var collection = db.collection('snapshots');
             collection.insertOne(obj, (result) => {
                 console.log('Successfully logged entry');
+            });
+        }
+    });
+}
+
+
+function select(res: Response, query: any) {
+    let mongoClient = new mongodb.MongoClient();
+    mongoClient.connect('mongodb://mongo:27017/opensights', (err: Error, db: mongodb.Db) => {
+        if (err) {
+            console.log(err);
+        } else {
+            var collection = db.collection('snapshots');
+            collection.aggregate([
+                { $match: {} }
+                , {
+                    $group:
+                    { _id: query, list: { $push: '$$ROOT' } }
+                }
+            ]).toArray((err: Error, results: any) => {
+                res.json(results);
+                db.close();
             });
         }
     });
