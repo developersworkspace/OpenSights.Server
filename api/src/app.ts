f5c06a94f78004b6621ac007ccfee9b741da4c8e
Brendan.Huysamen <developersworkspace@gmail.com>
2017-03-03 10:51:27 +0200

    adds logging and prod config

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,12 +3,13 @@ import express = require("express");
 import bodyParser = require("body-parser");
 import * as https from 'https';
 import * as fs from 'fs';
+import expressWinston = require('express-winston');
 
 // Import Routes
 import * as dataRouter from './routes/data';
 
 // Import middleware
-import { CORS } from './middleware/common';
+import * as cors from 'cors';
 
 // Imports logger
 import { logger } from './logger';
@@ -23,7 +24,12 @@ export class WebApi {
     private configureMiddleware(app: express.Express) {
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({ extended: false }));
-        app.use(CORS);
+        app.use(cors());
+        app.use(expressWinston.logger({
+            winstonInstance: logger,
+            meta: false,
+            msg: 'HTTP Request: {{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}'
+        }));
     }
 
     private configureRoutes(app: express.Express) {
